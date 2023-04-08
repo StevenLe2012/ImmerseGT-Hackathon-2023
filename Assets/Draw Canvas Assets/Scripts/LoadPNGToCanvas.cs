c8fdeb02492ff835b7fa06a112252d5f402745d0
Darius_sh711 <sh711@duke.edu>
2023-04-08 12:58:14 -0700

    checkout commit

--- Assets/Draw Canvas Assets/Scripts/LoadPNGToCanvas.cs
+++ Assets/Draw Canvas Assets/Scripts/LoadPNGToCanvas.cs
@@ -17,15 +17,21 @@ public class LoadPNGToCanvas : MonoBehaviour
 
     private void LoadPNGToNormalTexture() {
         string imagePath = Path.Combine(Application.persistentDataPath, "NewSavedScreen.png");
-        debugCube.GetComponent<MeshRenderer>().material.color = Color.red;
         byte[] imageData = File.ReadAllBytes(imagePath);
-        debugCube.GetComponent<MeshRenderer>().material.color = Color.green;
         Texture2D texture = new Texture2D(2048, 2048);
         texture.LoadImage(imageData);
+        // for (int y = 0; y < texture.height; y++) {
+        //     for (int x = 0; x < texture.width / 2; x++) {
+        //         Color temp = texture.GetPixel(x, y);
+        //         texture.SetPixel(x, y, texture.GetPixel(texture.width - x - 1, y));
+        //         texture.SetPixel(texture.width - x - 1, y, temp);
+        //     }
+        // }
+        // texture.Apply();
         Material material = GetComponent<MeshRenderer>().material;
         material.mainTexture = texture;
 
         // rotate the canvas locally by 180 degrees by y axis for offset
-        transform.Rotate(0.0f, 180.0f, 0.0f, Space.Self); 
+        // transform.Rotate(0.0f, 180.0f, 0.0f, Space.Self); 
     }
 }
