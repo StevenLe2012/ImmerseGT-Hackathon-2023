034c98b472a49aaa9ccfd265d79b219f14c5edb9
Steven Le <64396816+StevenLe2012@users.noreply.github.com>
2023-04-08 21:00:00 -0700

    added movement files

---
+++ Assets/_Scripts/Multiplayer/NetworkPlayer.cs
@@ -0,0 +1,70 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Netcode;
+using UnityEngine;
+using UnityEngine.SpatialTracking;
+using UnityEngine.XR.Hands.Samples.VisualizerSample;
+using UnityEngine.XR.Interaction.Toolkit;
+using Random = UnityEngine.Random;
+
+public class NetworkPlayer : NetworkBehaviour
+{
+    [SerializeField] private Vector2 placementArea = new Vector2(-10.0f, 10.0f);
+
+    public override void OnNetworkSpawn()
+    {
+        DisableClientInput();
+    }
+
+    private void DisableClientInput()
+    {
+        if (IsClient && !IsOwner)
+        {
+            var clientHandVisualizer = GetComponentInChildren<HandVisualizer>();
+            var clientHead = GetComponentInChildren<TrackedPoseDriver>();
+            var clientCamera = GetComponentInChildren<Camera>();
+
+            clientCamera.enabled = false;
+            clientHandVisualizer.enabled = false;
+            clientHead.enabled = false;
+        }
+    }
+
+    private void Start()
+    {
+        if (IsClient && IsOwner)
+        {
+            transform.position = new Vector3(Random.Range(placementArea.x, placementArea.y), transform.position.y,
+            Random.Range(placementArea.x, placementArea.y));
+        }
+    }
+
+    public void OnSelectGrabbable(SelectEnterEventArgs eventArgs)
+    {
+        if (IsClient && IsOwner)
+        {
+            NetworkObject networkObjectSelected = eventArgs.interactableObject.transform.GetComponent<NetworkObject>();
+
+            if (networkObjectSelected != null)
+            {
+                // request ownership from the server
+                RequestGrabbableOwnershipServerRpc(OwnerClientId, networkObjectSelected);
+            }
+        }
+    }
+
+    [ServerRpc]
+    private void RequestGrabbableOwnershipServerRpc(ulong newOwnerClientId, NetworkObjectReference networkObjectReference)
+    {
+        if (networkObjectReference.TryGet(out NetworkObject networkObject))
+        {
+            networkObject.ChangeOwnership(newOwnerClientId);
+        }
+        else
+        {
+            // Logger.LogWarning($"Unable to change ownership for clientId {newOwnerClientId}");
+            Debug.LogWarning($"Unable to change ownership for clientId {newOwnerClientId}");
+        }
+    }
+}
